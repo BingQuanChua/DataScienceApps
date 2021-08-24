3ba4c03ed76db869b92a20626f8bbed2b6d586f9
BingQuan <bingquan6102@gmail.com>
2021-08-24 15:13:13 +0800

    Trying to deploy web app

--- 04_eda_s&p500_stock_price/myapp.py
+++ 04_eda_s&p500_stock_price/myapp.py
@@ -114,7 +114,7 @@ def load_data():
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="S&P500.csv">Download CSV File</a>'
     return href
 
 if __name__ == "__main__":
